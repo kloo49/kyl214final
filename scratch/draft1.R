bfbb8a94028fde2c20248bd71833a2e0edf40b7e
Kelly Loo <loo.yqkelly@gmail.com>
2026-08-25 17:54:17 -0700

    added moving average

--- scratch/draft1.R
+++ scratch/draft1.R
@@ -60,7 +60,10 @@ ggplot(
   labs(
     title = "ion conc over time (not moving avg)"
   ) +
-  facet_wrap(~ions)
+  facet_wrap(
+    ~ions,
+    scales = "free",
+    ncol = 1)
 
 # units are not the same
 
@@ -75,25 +78,54 @@ max(big_frame_select_longer$Sample_Date)
 max(big_frame_select_longer$Sample_Date) - min(big_frame_select_longer$Sample_Date)
 # Time difference of 12642 days
 
-start_window <- seq(ymd("1986-05-20"),ymd("2020-12-29"), by = "7 days")
+window_start <- seq(ymd("1986-05-20"),ymd("2020-12-29"), by = "7 days")
 window_end <- window_start + days(7)
 tibble_mov_avg <- tibble(big_frame_select_longer$Sample_ID, window_start,window_end, "mean_K" = NA, "mean_Mg" = NA, "mean_Ca" = NA, "mean_no3-n" = NA, "mean_nh4-n" = NA)
 tibble_mov_avg <- tibble("site" = NA, window_start,window_end, "mean_K" = NA, "mean_Mg" = NA, "mean_Ca" = NA, "mean_no3-n" = NA, "mean_nh4-n" = NA)
 glimpse(tibble_mov_avg)
 glimpse(big_frame_select)
 
-for (i in nrow(tibble_mov_avg)) {
+for (i in 1:nrow(tibble_mov_avg)) {
   start_date <- window_start[i]
   end_date <- window_end[i]
-  mean_k <- mean(big_frame_select$K[start_date >= big_frame_select$Sample_Date & end_date < big_frame_select$Sample_Date])
+  site <- mean(big_frame_select$Sample_ID[start_date <= big_frame_select$Sample_Date & end_date > big_frame_select$Sample_Date])
+  tibble_mov_avg$site[i] <- site
+  mean_k <- mean(big_frame_select$K[start_date <= big_frame_select$Sample_Date & end_date > big_frame_select$Sample_Date])
   tibble_mov_avg$mean_K[i] <- mean_k
-  mean_ca <- mean(big_frame_select$Ca[start_date >= big_frame_select$Sample_Date & end_date < big_frame_select$Sample_Date])
+  mean_ca <- mean(big_frame_select$Ca[start_date <= big_frame_select$Sample_Date & end_date > big_frame_select$Sample_Date])
   tibble_mov_avg$mean_Ca[i] <- mean_ca
-  mean_mg <- mean(big_frame_select$Mg[start_date >= big_frame_select$Sample_Date & end_date < big_frame_select$Sample_Date])
+  mean_mg <- mean(big_frame_select$Mg[start_date <= big_frame_select$Sample_Date & end_date > big_frame_select$Sample_Date])
   tibble_mov_avg$mean_Mg[i] <- mean_mg
-  mean_nh4 <- mean(big_frame_select$`NH4-N`[start_date >= big_frame_select$Sample_Date & end_date < big_frame_select$Sample_Date])
+  mean_nh4 <- mean(big_frame_select$`NH4-N`[start_date <= big_frame_select$Sample_Date & end_date > big_frame_select$Sample_Date])
   tibble_mov_avg$'mean_nh4-n'[i] <- mean_nh4
-  mean_no3 <- mean(big_frame_select$`NO3-N`[start_date >= big_frame_select$Sample_Date & end_date < big_frame_select$Sample_Date])
+  mean_no3 <- mean(big_frame_select$`NO3-N`[start_date <= big_frame_select$Sample_Date & end_date > big_frame_select$Sample_Date])
   tibble_mov_avg$'mean_no3-n'[i] <- mean_no3
 }
 glimpse(tibble_mov_avg)
+
+tibble_mov_avg_longer <- tibble_mov_avg |> 
+  pivot_longer(
+    cols = "mean_K":"mean_nh4-n",
+    names_to = "mean_ions",
+    values_to = "mean_conc"
+  )
+
+ggplot(
+  data = tibble_mov_avg_longer,
+  mapping = aes(
+    x = window_start,
+    y = mean_conc,
+    color = mean_ions
+  )
+) +
+  geom_line() +
+  labs(
+    title = "Moving average of ions",
+    x = "Year",
+    y = "Concentration"
+  ) +
+  facet_wrap(
+    ~mean_ions,
+    scales = "free",
+    ncol = 1
+  )
